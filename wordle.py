8a6d1433d8ab83dbf4751854190794c029894558
tovawieds <155400443+tovawieds@users.noreply.github.com>
2024-02-25 16:32:56 +0000

    1

--- wordle.py
+++ wordle.py
@@ -24,7 +24,12 @@ if len(sys.argv) >= 2:
     target = sys.argv[1]
 else:
     # TODO choose a random word from valid_words
-    target = 'alter'  # <== change this
+    target = random.choice(list(valid_words))  # <== change this
+#########
+# # choose a word at random from the list of words
+# used_words = wordle_engine.load_words("shuffled_real_wordlist.txt")
+# target = random.choice(used_words)
+#########
 
 # TODO Implement the rest of the game.
 # Remember:
@@ -34,3 +39,7 @@ else:
 #   * Please display the entire history of guesses before each prompt.
 #   * Print a message at the end of the game indicating whether the player won or lost.
 #      * If the player wins, display the entire sequence of guesses as part of the final message.
+
+all_letters = wordle_engine.create_letter_status()
+for i in range(5):
+    guess = input(f"Make a guess ({wordle_engine.format_letters(all_letters)}): ")
--- wordle_engine.py
+++ wordle_engine.py
@@ -24,15 +24,28 @@ def welcome_string():
 def create_letter_status():
     """ Initialize and Return a new dictionary that maps each letter to
         wordle_colors.BLUE """
-    pass
-
+    # dictionary that will have the words
+    alpha = {}
+    # loop through the letters and add each to dictionary, mapping to the color blue
+    for letter in wordle_alphabet:
+        alpha[letter] = wordle_colors.BLUE
+    # return the new dictionary
+    return alpha
 
 def load_words(filename: str):
     """ Load the words from the specified file and place them
-        in a set. 
+        in a set.
         Ignore any lines that begin with "#"
         """
-    pass
+    # create a set to load the words into
+    words_set = set()
+    # open the file, if the line doesn't start with '#', add the word to the set
+    with open(filename) as file:
+        for line in file:
+            if line[0] != "#":
+                words_set.add(line)
+    # return the set of words
+    return words_set
 
 def format_guess(target, guess):
     """ Return a string that contains the user's guess formatted
@@ -42,7 +55,16 @@ def format_guess(target, guess):
                   but in a different location.
         * RED:    The letter does not appear in the target word
         Also, the string should end with wordle_colors.ENDC """
-    pass
+    new_word = ""
+    for letter in guess:
+        if letter in target:
+            if guess.index(letter) == target.index(letter):
+                new_word += f"{wordle_colors.GREEN}{letter}{wordle_colors.ENDC}"
+            else:
+                new_word += f"{wordle_colors.YELLOW}{letter}{wordle_colors.ENDC}"
+        else:
+            new_word += f"{wordle_colors.RED}{letter}{wordle_colors.ENDC}"
+    return new_word
 
 def update_letter_status(letter_status, target, guess):
     """ Update the letter status dictionary to show which letters
@@ -54,10 +76,40 @@ def update_letter_status(letter_status, target, guess):
                   (but not in the correct location)
         * RED:    Letter does  not appear in the target word, but has
                   been used in at least one guess."""
-    pass
+    # loop through each letter in guess
+    for letter in guess:
+        # if the letter appears in the targeted word, check ehere it is
+        if letter in target:
+            # if the letter is in the right spot, ake it green
+            if guess.index(letter) == target.index(letter):
+                letter_status[letter] = wordle_colors.GREEN
+            # if the letter is in the wrong spot, make it yellow
+            else:
+                letter_status[letter] = wordle_colors.YELLOW
+        # if the letter does not appear at all, make it red
+        else:
+            letter_status[letter] = wordle_colors.RED
 
 def format_letters(alphabet_dict):
     """ Generate a string that lists all the letters of the alphabet
         colored according to the rules given in update_letter_status.
         the string should end with wordle_colors.ENDC """
-    pass
+    new_alpha = ""
+    for letter in alphabet_dict:
+        new_alpha += alphabet_dict[letter] + letter
+    return new_alpha
+
+letters = create_letter_status()
+print(format_letters(letters))
+
+
+
+
+
+
+# letters = create_letter_status()
+# print(letters)
+# update_letter_status(letters, "hello", "steak")
+# for letter in letters:
+#     print(letters[letter] + letter)
+# print('\033[94m' + "a")
